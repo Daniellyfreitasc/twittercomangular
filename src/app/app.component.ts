2ec818c8cf907026fb201b180c4acb85dd9370b4
Danielly <daniellyfreitasc@hotmail.com>
2024-02-14 15:51:43 -0300

    data-biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, HomeComponent]
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, FormsModule]
 })
 export class AppComponent {
   title = 'threads';
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
 export class PostComponent {
+  tweetMessage: string = ''
+  tweetMaxLength: number = 240
+  tweetRemain: number = this.tweetMaxLength
 
+  sendTweet(){
+    alert("Twitter Criado!")
+  }
+
+  changeTweet(){
+    this.tweetRemain = 240 - this.tweetMessage.length
+  }
 }
