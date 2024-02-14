63d7f410274e5579e98642475953c1cec13691d5
Danielly <daniellyfreitasc@hotmail.com>
2024-02-14 10:03:28 -0300

    footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
-
+import { FooterComponent } from '../../components/footer/footer.component';
+import { PostComponent } from "../../components/post/post.component";
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent,
+        FooterComponent, PostComponent]
 })
 export class HomeComponent {
 
